88d2db47992674abdaa560ab951df75b44c7e0b6
Jennifer Stokes <jenniferstokes@Jennifers-MacBook-Pro.local>
2017-12-08 17:21:34 -0800

    adding HTML and assets

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBQp2qzcmnXE28wdLXHcvpfoQ7JPbyEmAs",
+    authDomain: "train-time-19885.firebaseapp.com",
+    databaseURL: "https://train-time-19885.firebaseio.com",
+    projectId: "train-time-19885",
+    storageBucket: "train-time-19885.appspot.com",
+    messagingSenderId: "140676448999"
+  };
+  firebase.initializeApp(config);
