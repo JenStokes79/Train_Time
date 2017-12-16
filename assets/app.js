ab372a30987aa07305f23f016d8469283ce09737
Jennifer Stokes <jenniferstokes@Jennifers-MacBook-Pro.local>
2017-12-15 19:29:37 -0800

    no changes

--- assets/app.js
+++ assets/app.js
@@ -63,37 +63,25 @@ console.log(destination);
 console.log(firstTrain);
 console.log(frequency);
 
-//var firstTrainPretty = moment.(firstTrain).format("HHmm");
+
 
 var currentTime = moment();
 
 
-//var nextArrival = moment().diff(moment(currentTime), "minutes");
-//console.log(nextArrival);
-
-
- //var currentTime = moment().format("HH:mm")
-  //console.log("Local Current Time: " + currentTime)
- //var timeDifference = moment(currentTime).add(frequency, "minutes").format("HH:mm");
- // console.log("Time Difference from Current Time to Next Train: " + timeDifference);
-
+//math for train (had to look this up)
 var difference = moment().diff(moment.unix(firstTrain), "minutes");
 var remainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
 var newTime = frequency - remainder;
 
-var nextArrival = moment().add(newTime, "m").format("hh:mm"); 
+var nextArrival = moment().add(newTime, "m").format("HH:mm"); 
 
 
 console.log(difference);
 console.log(remainder);
 console.log(newTime);
 console.log(nextArrival);
-//var newTime = moment().add(frequency, "minutes").format("HH:mm");
-//console.log(newTime)
 
-//var difference = frequency - newTime;
-//console.log(difference);
-
+  //pushes data into train table
  $("#train-table > tbody").append("<tr><td>" + train + "</td><td>" + destination + "</td><td>" +
   frequency + "</td><td>" + nextArrival + "</td><td>" + newTime + "</td><td>");
 });
